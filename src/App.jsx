5507ec39dc763a3097cdebc0720b6a4db35c7802
RORN Makara <rornmakara89@gmail.com>
2025-10-16 11:18:49 +0700

    initail

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">POS System 💻</h1>
+      <p className="text-gray-700 mt-4">Welcome to your new React + Vite app!</p>
+    </div>
+  )
+}
+
+export default App
